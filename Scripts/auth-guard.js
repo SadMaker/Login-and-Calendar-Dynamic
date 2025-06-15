3849db1cb82236d83c4d367707054fd23bd8e91e
SadMaker <60515441+SadMaker@users.noreply.github.com>
2025-06-15 03:04:39 -0300

    Update auth-guard.js

--- Scripts/auth-guard.js
+++ Scripts/auth-guard.js
@@ -1,5 +1,28 @@
 firebase.auth().onAuthStateChanged(user => {
-    if(!user){
-        window.location.href = "/index.html";
+    if (user) {
+        const rememberMe = localStorage.getItem("rememberMe") === "true";
+        // Verifica se a URL atual NÃO termina com "index/pages/home.html"
+        const isNotHomePage = !window.location.pathname.endsWith("index/pages/home.html") && !window.location.pathname.endsWith("home.html");
+
+        if (!rememberMe && isNotHomePage) {
+            firebase.auth().signOut().then(() => {
+                console.log("Usuário deslogado porque rememberMe é falso e não está na home.");
+                // window.location.href = "/index.html"; 
+            }).catch(error => {
+                console.error("Erro ao deslogar usuário:", error);
+            });
+        }
+    } else {
+        const nonAuthPages = ["/index.html", "/index/pages/register.html", "/index/pages/recuperation.html"];
+        const isNonAuthPage = nonAuthPages.some(page => window.location.pathname.endsWith(page));
+        
+        const isHomePage = window.location.pathname.endsWith("index/pages/home.html") || window.location.pathname.endsWith("home.html");
+
+        if (!isNonAuthPage && !isHomePage) {
+             window.location.href = "/index.html";
+        } else if (isHomePage) {
+            window.location.href = "/index.html";
+        }
     }
 });
+
